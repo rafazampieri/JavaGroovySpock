90b381e648c76d914a116775d8b4fa3634d8b938
rafazampieri <r_zampieri@hotmail.com>
2015-10-11 16:44:14 -0300

    sync

--- src/test/java/com/rafazampieri/spock/MathUtilsTest.java
+++ src/test/java/com/rafazampieri/spock/MathUtilsTest.java
@@ -1,14 +1,16 @@
 package com.rafazampieri.spock;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
+import java.util.Collection;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
+// https://github.com/junit-team/junit/wiki/Parameterized-tests
 @RunWith(value= Parameterized.class)
 public class MathUtilsTest {
 	
@@ -30,7 +32,7 @@ public class MathUtilsTest {
 	}
 	
 	@Parameters(name = "{index}: add( {0}, {1} ) = {2}")
-	public static Iterable<Object[]> getData(){
+	public static Collection<Object[]> getData(){
 		return Arrays.asList(new Object[][]{
 				{1, 2, 3},
 				{5, 6, 11}
